2e38bcd18a32d38f88f1baa6d43e765b2bbfb77c
MarinaSigida <sigida.web@gmail.com>
2023-07-16 12:12:25 +0200

    finish

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,19 +1,31 @@
 import css from './FriendList.module.css';
 import PropTypes from 'prop-types';
-import { Friend } from './Friend/Friend';
+import { FriendListItem } from './FriendListItem/FriendListItem';
 
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendListItem
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+            key={id}
+          />
+        );
+      })}
+    </ul>
+  );
+};
 
-export const FriendList = ({friends})=>{
-return <ul className={css.friendList}>
-{friends.map(({ avatar, name, isOnline, id }) => {
-return <Friend avatar={avatar} name={name} isOnline={isOnline} key={id} />;
-})}
-</ul>
-}
-
-FriendList.propTypes  = {
-    friends: PropTypes.arrayOf(
-        PropTypes.exact({
-            id:PropTypes.number.isRequired,
-        }))
-}
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.bool,
+    }).isRequired
+  ).isRequired,
+};
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,7 +9,7 @@ export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
       {title && (
-        <h2 className={css.title} style={{ backgroundColor: 'teal' }}>
+        <h2 className={css.title}>
           {title}
         </h2>
       )}
